dc042b62267d1f32c834bb5c20c2919932dd82e7
lkisac <len_isac@hotmail.com>
2015-02-10 13:59:02 -0500

    removed selenium-commons from pom, selenium create job and configure up to 'complex param..'

--- test/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinitionTestTwo.java
+++ test/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinitionTestTwo.java
@@ -6,13 +6,25 @@ package com.cwctravel.hudson.plugins.extended_choice_parameter;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.FreeStyleBuild;
+import hudson.model.AbstractBuild;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.Shell;
+import hudson.util.OneShotEvent;
+
+import org.apache.commons.io.FileUtils;
 import org.easymock.EasyMock;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.Rule;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+import org.jvnet.hudson.test.HudsonTestCase;
 //import org.mockito.Mock;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.TestBuilder;
 
 /**
  * @author lkisac
@@ -21,26 +33,70 @@ import org.junit.Test;
 public class ExtendedChoiceParameterDefinitionTestTwo {
 
 	// mock creation
-	//@Mock
-	private static ExtendedChoiceParameterDefinition mockedExtendedChoiceObj = EasyMock.createMock(ExtendedChoiceParameterDefinition.class);
-	
+	// @Mock
+	// private static ExtendedChoiceParameterDefinition mockedExtendedChoiceObj
+	// = EasyMock.createMock(ExtendedChoiceParameterDefinition.class);
+
+	// Rule
+	@Rule
+	public JenkinsRule j = new JenkinsRule();
+
+	@Test
+	public void first() throws Exception {
+		FreeStyleProject project = j.createFreeStyleProject();
+		project.getBuildersList().add(new Shell("echo hello"));
+		FreeStyleBuild build = project.scheduleBuild2(0).get();
+		System.out.println(build.getDisplayName() + " completed");
+		// TODO: change this to use HtmlUnit
+		//String s = FileUtils.readFileToString(build.getLogFile());
+		//assertThat(s, contains("+ echo hello"));
+	}
+
+	@Test
+	public void testCustomBuilder() throws IOException, InterruptedException {
+		final OneShotEvent buildStarted = new OneShotEvent();
+
+		FreeStyleProject project = j.createFreeStyleProject("Extended-Test-Custom");
+		project.getBuildersList().add(new TestBuilder() {
+
+			@Override
+			public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
+					BuildListener listener) throws InterruptedException, IOException {
+				build.getWorkspace().child("abc.txt").write("hello", "UTF-8");
+				return true;
+			}
+		});
+
+		project.scheduleBuild2(0);
+		buildStarted.block();
+
+		// assertTrue(buildStarted.equals(true));
+	}
+
+	@Test
+	public void testAdd() {
+		String str = "test";
+		assertEquals("test", str);
+	}
+
 	@Test
 	public void testLogin() {
-		
+
 	}
-	
+
 	public void testConfigure() {
-//		FreeStyleProject project = j.createFreeStyleProject();
-//		FreeStyleProject p = j.createFreeStyleProject();
-//		Builder before = new Builder("a", "b", true, 100);
-//
-//		p.getBuildersList().add(before);
-//
-//		j.submit(j.createWebClient().getPage(p, "configure").getFormByName("config"));
-//
-//		Builder after = p.getBuildersList().get(Builder.class);
-//
-//		j.assertEqualBeans(before, after, "prop1,prop2,prop3,...");
+		// FreeStyleProject project = j.createFreeStyleProject();
+		// FreeStyleProject p = j.createFreeStyleProject();
+		// Builder before = new Builder("a", "b", true, 100);
+		//
+		// p.getBuildersList().add(before);
+		//
+		// j.submit(j.createWebClient().getPage(p,
+		// "configure").getFormByName("config"));
+		//
+		// Builder after = p.getBuildersList().get(Builder.class);
+		//
+		// j.assertEqualBeans(before, after, "prop1,prop2,prop3,...");
 
 		// project.getBuildersList().add(new Shell("echo hello"));
 		// FreeStyleBuild build = project.scheduleBuild2(0).get();
@@ -52,52 +108,7 @@ public class ExtendedChoiceParameterDefinitionTestTwo {
 		// // System.exit(0);
 		// Assert.assertEquals("HtmlUnit - Welcome to HtmlUnit",
 		// page.getTitleText());
-		
-	}
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
 
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition#createValue(org.kohsuke.stapler.StaplerRequest, net.sf.json.JSONObject)}.
-	 */
-	@Test
-	public void testCreateValueStaplerRequestJSONObject() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link hudson.model.ParameterDefinition#createValue(org.kohsuke.stapler.StaplerRequest, net.sf.json.JSONObject)}.
-	 */
-	@Test
-	public void testCreateValueStaplerRequestJSONObject1() {
-		fail("Not yet implemented");
 	}
 
 }
--- test/com/cwctravel/hudson/plugins/extended_choice_parameter/SeleniumWebDriver.java
+++ test/com/cwctravel/hudson/plugins/extended_choice_parameter/SeleniumWebDriver.java
@@ -9,6 +9,7 @@
 package com.cwctravel.hudson.plugins.extended_choice_parameter;
 
 import java.io.File;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,35 +24,110 @@ public class SeleniumWebDriver {
 		File file = new File("C:/WebDrivers/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		WebDriver driver = new ChromeDriver();
+		WebElement element;
+		Select select;
 
 		// Open Jenkins home page
 		driver.get("http://localhost:9090");
 		Thread.sleep(2000);
 
 		// Login
-		WebElement link;
-		link = driver.findElement(By.linkText("log in"));
-		link.click();
+		driver.findElement(By.linkText("log in")).click();
 		Thread.sleep(2000);
-		WebElement textBox;
-		textBox = driver.findElement(By.id("j_username"));
-		textBox.sendKeys("admin");
+		driver.findElement(By.id("j_username")).sendKeys("admin");
 		Thread.sleep(2000);
-		textBox = driver.findElement(By.name("j_password"));
-		textBox.sendKeys("admin");
-		link = driver.findElement(By.id("yui-gen1-button"));
-		link.click();
+		driver.findElement(By.name("j_password")).sendKeys("admin");
+		driver.findElement(By.id("yui-gen1-button")).click();
 		Thread.sleep(2000);
 
-		// Build Extended-Test job
-		link = driver.findElement(By.linkText("Extended-Test"));
-		link.click();
+		// Create new "Extended-Test" job
+		driver.findElement(By.linkText("New Item")).click();
 		Thread.sleep(2000);
-		link = driver.findElement(By.linkText("Build with Parameters"));
-		link.click();
+		driver.findElement(By.name("name")).sendKeys("Extended-Test-Auto");
+		driver.findElement(By.xpath("//input[@value='hudson.model.FreeStyleProject']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("ok-button")).click();
+		Thread.sleep(2000);
+
+		// Configure job
+		driver.findElement(By.name("parameterized")).click(); // Check 'This
+																// build is
+																// parameterized'
+		Thread.sleep(2000);
+		driver.findElement(By.className("hetero-list-add")).click(); // Add
+																		// parameter
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Extended Choice Parameter")).click(); // Select
+																				// parameter
+		Thread.sleep(2000);
+		driver.findElement(By.name("_.name")).sendKeys("Country_2_Levels");
+		Thread.sleep(2000);
+		driver.findElement(
+				By.xpath("//input[@type='radio'][./ancestor-or-self::label[contains(., 'Complex Parameter Types')]]"))
+				.click(); // Select 'Complex Parameter Types'
+		Thread.sleep(2000);
+
+		// Select option where select list is invisible
+
+		//element = driver.findElement(By.xpath("//select[@name='type']"));
+		//element.isEnabled();
+		
+		int size = driver.findElements(By.xpath("//select[@name='type']")).size();
+		for (int i = 0; i < size; i++) {
+			element = driver.findElements(By.xpath("//select[@name='type']")).get(i);
+			if (element.isDisplayed()) {
+				select = new Select(element);
+				select.selectByVisibleText("Multi-Level Multi Select");
+				break;
+			}
+		}
+		
+//		select = new Select(
+//				driver.findElement(By
+//						.xpath("//select[@name='type'][./ancestor::tr[@class='radio-block-start row-set-start']][./descendant::label[contains(., 'Complex Parameter Types')]]")));
+//		select.selectByVisibleText("Multi-Level Multi Select");
+		// List<WebElement> options = select.getOptions();
+		// for(WebElement option : options)
+		// {
+		// if(
+		// option.getAttribute("value").equals("PT_MULTI_LEVEL_MULTI_SELECT") )
+		// {
+		// option.click();
+		// break;
+		// }
+		// }
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//button[@type='button'][contains(., 'Add build step')]"))
+				.click(); // Add Build Step
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Execute Shell")).click(); // Execute
+																	// Shell
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//textarea[@name='command']")).sendKeys(
+				"# Create config files" + "echo 'Country	City" + "United States	San Francisco"
+						+ "United States	Chicago" + "Mexico	Mexico City"
+						+ "Mexico	Cancun' > countries.txt" + "echo 'Country	City	Street"
+						+ "United States	San Francisco	Street1" + "United States	Chicago	Street2"
+						+ "Mexico	Mexico City	Street3" + "Mexico	Cancun	Street4' > countries1.txt"
+						+ "echo 'Country	City	Street	Number"
+						+ "United States	San Francisco	Street1	11"
+						+ "United States	Chicago	Street2	12" + "Mexico	Mexico City	Street3	13"
+						+ "Mexico	Cancun	Street4	14' > countries2.txt");
+		Thread.sleep(2000);
+
+		// driver.findElement(By.name("_.propertyFile"));
+
+		driver.close();
+		driver.quit();
+
+		// Build Extended-Test job
+		driver.findElement(By.linkText("Extended-Test-Auto")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Build with Parameters")).click();
 		Thread.sleep(2000);
 		// Select from dropdown lists
-		Select select = new Select(driver.findElement(By
+		select = new Select(driver.findElement(By
 				.id("Country_2_Levels dropdown MultiLevelMultiSelect 0")));
 		select.selectByVisibleText("United States");
 		Thread.sleep(2000);
@@ -59,8 +135,7 @@ public class SeleniumWebDriver {
 				.id("Country_2_Levels dropdown MultiLevelMultiSelect 0 United States")));
 		select.selectByVisibleText("San Francisco");
 		Thread.sleep(2000);
-		link = driver.findElement(By.id("Country_2_Levels addAnotherButton"));
-		link.click();
+		driver.findElement(By.id("Country_2_Levels addAnotherButton")).click();
 		Thread.sleep(2000);
 		select = new Select(driver.findElement(By
 				.id("Country_2_Levels dropdown MultiLevelMultiSelect 1")));
@@ -71,13 +146,11 @@ public class SeleniumWebDriver {
 		select.selectByVisibleText("Mexico City");
 		Thread.sleep(2000);
 		// Build
-		link = driver.findElement(By.id("yui-gen1-button"));
-		link.click();
+		driver.findElement(By.id("yui-gen1-button")).click();
 		Thread.sleep(10000);
 
 		// Check Console Output
-		link = driver.findElement(By.className("build-status-link"));
-		link.click();
+		driver.findElement(By.className("build-status-link")).click();
 		Thread.sleep(10000);
 
 		// Done
