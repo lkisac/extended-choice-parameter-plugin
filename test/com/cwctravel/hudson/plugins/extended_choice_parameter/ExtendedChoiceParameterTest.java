6165eb0267bb707533c6dd27459eb1dd557fa334
lkisac <len_isac@hotmail.com>
2015-02-25 09:53:51 -0500

    fixed package structure main.java.com -> com.

--- test/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterTest.java
+++ test/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterTest.java
@@ -2,7 +2,7 @@
  * This Test will test an existing Extended-Test job with Extended Choice Parameter
  * multi-level select list to test storing multiple values
  */
-package main.java.com.cwctravel.hudson.plugins.extended_choice_parameter;
+package com.cwctravel.hudson.plugins.extended_choice_parameter;
 
 import java.io.IOException;
 
