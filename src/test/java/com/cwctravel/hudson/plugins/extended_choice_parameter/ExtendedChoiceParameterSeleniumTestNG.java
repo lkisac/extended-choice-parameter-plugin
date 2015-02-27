2fc5489a594405dbd1392f597f360a0f34084cbe
lkisac <len_isac@hotmail.com>
2015-02-27 17:41:16 -0500

    finished config, just have to run build to create config file then go to build screen again

--- src/test/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterSeleniumTestNG.java
+++ src/test/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterSeleniumTestNG.java
@@ -3,6 +3,9 @@ package com.cwctravel.hudson.plugins.extended_choice_parameter;
 //import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -25,6 +28,7 @@ public class ExtendedChoiceParameterSeleniumTestNG {
 		WebDriver driver = new ChromeDriver(options);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		WebElement element;
+		List<WebElement> elements;
 		Select select;
 
 		// Open Jenkins home page
@@ -69,12 +73,27 @@ public class ExtendedChoiceParameterSeleniumTestNG {
 				select = new Select(element);
 				select.selectByVisibleText("Multi-Level Multi Select");
 				Thread.sleep(2000);
-				element = driver.findElement(By.xpath("//input[@name='_.propertyFile']"));
-				//((JavascriptExecutor)driver).executeScript("alert(arguments[0])", element);
-//				Thread.sleep(7000);
-//				((JavascriptExecutor)driver).executeScript("arguments[1].style.visibility = 'visible';", element);
+				
+				// Property File
+				elements = driver.findElements(By.name("_.propertyFile"));
+				Iterator<WebElement> iter = elements.iterator();
+				while(iter.hasNext()) {
+				    element = iter.next();
+				}				
+				//((JavascriptExecutor)driver).executeScript("document.getElementByXpath(\"//input[@name='_.propertyFile']\")");
 				Thread.sleep(1000);
 				element.sendKeys("http://localhost:9090/job/Extended-Test-Auto/ws/countries.txt");
+				
+				// Property Value
+				Thread.sleep(2000);
+				elements = driver.findElements(By.name("_.propertyValue"));
+				iter = elements.iterator();
+				while(iter.hasNext()) {
+				    element = iter.next();
+				}				
+				//((JavascriptExecutor)driver).executeScript("document.getElementByXpath(\"//input[@name='_.propertyFile']\")");
+				Thread.sleep(1000);
+				element.sendKeys("Country,City");				
 				break;
 			}
 		}
@@ -92,17 +111,19 @@ public class ExtendedChoiceParameterSeleniumTestNG {
 		driver.findElement(By.xpath("//textarea[@name='command']"))
 				.sendKeys(
 						"# Create config files\necho 'Country\\tCity\\nUnited States\\tSan Francisco\\nUnited States\\tChicago\\nMexico\\tMexico City\\nMexico\\tCancun' > countries.txt\n");
-		// \'Country\tCity\nUnited States\tSan Francisco\nUnited States\tChicago\nMexico\tMexico City\nMexico\tCancun\' > countries.txt\n
 		Thread.sleep(2000);
 
 		// Save Job
 		driver.findElement(By.xpath("//button[contains(., 'Save')]")).click();
+		
+		Thread.sleep(3000);
 		
 		// Build Extended-Test job
 //		driver.findElement(By.linkText("Extended-Test-Auto")).click();
 //		Thread.sleep(2000);
 		driver.findElement(By.linkText("Build with Parameters")).click();
 		Thread.sleep(2000);
+		
 		// Select from dropdown lists
 		select = new Select(driver.findElement(By
 				.id("Country_2_Levels dropdown MultiLevelMultiSelect 0")));
