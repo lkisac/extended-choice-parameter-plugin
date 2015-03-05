cacabfde895af995d80cbb3bc078b7bc3689f033
lkisac <len_isac@hotmail.com>
2015-03-05 11:00:23 -0500

    testing JSONObject value with sysout

---
+++ src/test/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterMock.java
@@ -0,0 +1,10 @@
+package com.cwctravel.hudson.plugins.extended_choice_parameter;
+
+import org.testng.annotations.Test;
+
+public class ExtendedChoiceParameterMock {
+  @Test
+  public void f() {
+	  
+  }
+}
