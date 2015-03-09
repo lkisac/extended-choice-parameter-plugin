654b44974b04950979b6a70004012a3de2e5c00e
lkisac <len_isac@hotmail.com>
2015-03-09 15:11:36 -0400

    changed test class name to ExtendedChoiceParameterDefinitionTest

--- src/test/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinitionTest.java
+++ src/test/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinitionTest.java
@@ -16,8 +16,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.kohsuke.stapler.StaplerRequest;
 
-public class ExtendedChoiceParameterMock {
+public class ExtendedChoiceParameterDefinitionTest {
 	
+	/**
+	 * Check for concatenated value output from 
+	 * Multi-level select list 2 levels (Country,City)
+	 */
 	@Test
 	public void testCreateValueTwoLevels() {
 		ExtendedChoiceParameterDefinition definitionObj = new ExtendedChoiceParameterDefinition("Country_2_Levels", "PT_MULTI_LEVEL_MULTI_SELECT", "Country,City", null, null, null, null, null, null, null, null, null, null, null, false, 0, null, null);
@@ -41,21 +45,23 @@ public class ExtendedChoiceParameterMock {
 		Assert.assertEquals("1:United States,San Francisco:2:Mexico,Mexico City:", strParamValue.getValue().toString());
 	}
 
-	
+	/**
+	 * Multi-level select list 3 levels (Country,City,Hotel)
+	 */
 	@Test
 	public void testCreateValueThreeLevels() {
-		ExtendedChoiceParameterDefinition definitionObj = new ExtendedChoiceParameterDefinition("Country_3_Levels", "PT_MULTI_LEVEL_MULTI_SELECT", "Country,City,Street", null, null, null, null, null, null, null, null, null, null, null, false, 0, null, null);
+		ExtendedChoiceParameterDefinition definitionObj = new ExtendedChoiceParameterDefinition("Country_3_Levels", "PT_MULTI_LEVEL_MULTI_SELECT", "Country,City,Hotel", null, null, null, null, null, null, null, null, null, null, null, false, 0, null, null);
 		StaplerRequest request = EasyMock.createMock(StaplerRequest.class);
 		JSONObject jsonObj = new JSONObject();
 		JSONArray jsonArr = new JSONArray();
 		
-		jsonObj.put("name", "Country_2_Levels");
+		jsonObj.put("name", "Country_3_Levels");
 		jsonArr.add("United States");
 		jsonArr.add("San Francisco");
-		jsonArr.add("Street1");
+		jsonArr.add("Redwood Inn");
 		jsonArr.add("Mexico");
 		jsonArr.add("Mexico City");
-		jsonArr.add("Street3");
+		jsonArr.add("Gran Hotel");
 		jsonObj.put("value", jsonArr);
 		
 		//System.out.println("jsonArr: " + jsonArr.toString());
@@ -65,6 +71,6 @@ public class ExtendedChoiceParameterMock {
 		
 		//System.out.println("strParamValue: " + strParamValue.getValue());
 
-		Assert.assertEquals("1:United States,San Francisco,Street1:2:Mexico,Mexico City,Street3:", strParamValue.getValue().toString());
+		Assert.assertEquals("1:United States,San Francisco,Redwood Inn:2:Mexico,Mexico City,Gran Hotel:", strParamValue.getValue().toString());
 	}
 }
